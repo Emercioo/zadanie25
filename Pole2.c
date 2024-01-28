fba1da5f9812f5b4815563ee7397f298b44e8a1e
Damian Ludwiczak <damian.ludwiczak2001@gmail.com>
2024-01-28 20:23:21 +0100

    Finalna rewizja

--- Pole2.c
+++ Pole2.c
@@ -8,22 +8,47 @@ float c = 0;
 float r = 0;
 float h = 0;
 
-float obliczenieObjetosciProstopadloscianu(float a, float b, float c){
-    return(a*b*c);
+int wybor = 0;
+
+void obliczenieObjetosciProstopadloscianu(){
+    printf("Podaj pierwszy bok prostopadloscianu:\t");
+    scanf("%f", &a);
+    printf("Podaj drugi bok prostopadloscianu:\t");
+    scanf("%f", &b);
+    printf("Podaj trzeci bok prostopadloscianu:\t");
+    scanf("%f", &c);
+    printf("Objetosc prostopadlsocianu wynosi:\t%f\n", (a*b*c));
 }
 
-float obliczenieObjetosciWalca(float r, float h){
-    return(M_PI*pow(r, 2)*h);
-}
-
-
-int main() {
-
+void obliczenieObjetosciWalca(){
     printf("Podaj wysokosc walca:\t");
     scanf("%f", &h);
     printf("Podaj promien walca:\t");
     scanf("%f", &r);
-    printf("Objetosc walca wynosi:\t%f\n", obliczenieObjetosciWalca(r, h));
+    printf("Objetosc walca wynosi:\t%f\n", M_PI*pow(r, 2)*h);
+}
+
+
+int main() {
+    printf("Wybierz co chcesz obliczyc!\n");
+	printf("1 - Objetosc prostopadloscianu!\n");
+	printf("2 - Objetosc walca!\n");
+	printf("3 - Wyjscie!\n\n");
+	scanf("%d", &wybor);
+	switch(wybor){
+        case 1:
+            obliczenieObjetosciProstopadloscianu();
+            break;
+        case 2:
+            obliczenieObjetosciWalca();
+            break;
+        case 3:
+            return 0;
+        default:
+            printf("\nZla komenda!");
+	}
+
+
 
     return 0;
 }
