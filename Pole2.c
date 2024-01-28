90677a2765c1c9d41bd5df60b553e1fcc26db977
Alex <alekaytokarek80@gmail.com>
2024-01-28 20:15:05 +0100

    Rewizja walec

--- Pole2.c
+++ Pole2.c
@@ -5,20 +5,25 @@ float a = 0;
 float b = 0;
 float c = 0;
 
+float r = 0;
+float h = 0;
+
 float obliczenieObjetosciProstopadloscianu(float a, float b, float c){
     return(a*b*c);
+}
+
+float obliczenieObjetosciWalca(float r, float h){
+    return(M_PI*pow(r, 2)*h);
 }
 
 
 int main() {
 
-    printf("Podaj pierwszy bok prostopadloscianu:\t");
-    scanf("%f", &a);
-    printf("Podaj drugi bok prostopadloscianu:\t");
-    scanf("%f", &b);
-    printf("Podaj trzeci bok prostopadloscianu:\t");
-    scanf("%f", &c);
-    printf("Objetosc prostopadlsocianu wynosi:\t%f\n", obliczenieObjetosciProstopadloscianu(a,b,c));
+    printf("Podaj wysokosc walca:\t");
+    scanf("%f", &h);
+    printf("Podaj promien walca:\t");
+    scanf("%f", &r);
+    printf("Objetosc walca wynosi:\t%f\n", obliczenieObjetosciWalca(r, h));
 
     return 0;
 }
